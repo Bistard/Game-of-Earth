597cee491f0b5111aa48f04c32e46d2e814c106f
SIHAN LI <1015813038@qq.com>
2021-09-18 00:50:46 +0800

    init entity.ts

---
+++ src/world/entity/entity.ts
@@ -0,0 +1,25 @@
+import { IPosition } from "../../common/domNode.js";
+
+export type AnimalType = 'Human' | 'Rabbit' | 'Wolf' | 'Bear';
+export type StaticType = 'Grass' | 'Cloud';
+
+export type EntityType = AnimalType | StaticType;
+
+export interface IEntity {
+
+    readonly type: EntityType;
+    readonly position: IPosition;
+
+}
+
+export class Entity implements IEntity {
+
+    readonly type: EntityType;
+    readonly position: IPosition;
+
+    constructor(type: EntityType, position: IPosition) {
+        this.type = type;
+        this.position = position;
+    }
+
+}
