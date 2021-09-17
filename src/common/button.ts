c5c3d3c603f6c04789bfdec42a3001ecb2908025
SIHAN LI <1015813038@qq.com>
2021-09-17 21:55:22 +0800

    init button.ts

---
+++ src/common/button.ts
@@ -0,0 +1,52 @@
+import { IWidget } from "src/common/widget";
+
+export interface IButtonStyles {
+
+}
+
+export interface IButtonOptions extends IButtonStyles {
+
+}
+
+export interface IButton extends IWidget {
+
+}
+
+export class Button implements IButton {
+    
+    public readonly id: string;
+    public element: HTMLElement;
+    public imgElement?: HTMLImageElement;
+
+    constructor(id: string, container: HTMLElement) {
+        this.id = id;
+        this.element = document.createElement('div');
+        this.element.id = id;
+        
+        container.appendChild(this.element);
+    }
+
+    public setClass(classes: string[]): void {
+        this.element.classList.add(...classes);
+    }
+
+    public setImage(src: string): void {
+        this.imgElement = document.createElement('img');
+        this.imgElement.src = src;
+        
+        this.element.appendChild(this.imgElement);
+    }
+
+    public setImageID(id: string): void {
+        if (this.imgElement) {
+            this.imgElement.id = id;
+        }
+    }
+
+    public setImageClass(classes: string[]): void {
+        if (this.imgElement) {
+            this.imgElement.classList.add(...classes);
+        }
+    }
+
+}
---
+++ src/common/widget.ts
@@ -0,0 +1,5 @@
+export interface IWidget {
+    id?: string;
+    element: HTMLElement;
+    imgElement?: HTMLImageElement;
+}
