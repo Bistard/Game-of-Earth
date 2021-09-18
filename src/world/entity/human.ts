365b5073f439619d4b773004347e5a1049dea64f
SIHAN LI <1015813038@qq.com>
2021-09-18 17:41:20 +0800

    init human.ts

---
+++ src/world/entity/human.ts
@@ -0,0 +1,27 @@
+import { IPosition } from "../../common/UI/domNode.js";
+import { LivingType } from "./entity.js";
+import { LivingEntity } from "./livingEntity.js";
+
+export class Human extends LivingEntity {
+
+    constructor(parentContainer: HTMLElement, position: IPosition) {
+        super(LivingType.HUMAN, position, parentContainer, document.createElement('div'));
+        this.parentContainer.appendChild(this.container);
+        this._render();
+    }
+
+    public override update(): void {
+        
+        // DEBUG
+        this._moveTo({x: this.position.x + 0.2, y: this.position.y + 0.2});
+
+    }
+
+    private _render(): void {
+
+        this.container.classList.add('human-entity');
+        this._moveTo(this.position);
+
+    }
+
+}
