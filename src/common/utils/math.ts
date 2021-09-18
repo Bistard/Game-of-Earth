5ae6bc2c884c6348d48855d086ee7b5add0cb53c
小马奔腾 <leozhou2015@gmail.com>
2021-09-18 13:14:20 -0700

    util for unit vector calculation

--- src/common/utils/math.ts
+++ src/common/utils/math.ts
@@ -3,3 +3,10 @@ import { IPosition } from "../UI/domNode.js";
 export function calcDistance(pos1: IPosition, pos2: IPosition): number {
     return Math.sqrt((pos1.x - pos2.x)^2 + (pos1.y - pos2.y)^2);
 }
+
+export function getUnitVec(from: IPosition, target: IPosition) : [number, number] {
+    let deltaX = target.x - from.x;
+    let deltaY = target.y - from.y;
+    let distance = calcDistance(from, target);
+    return [deltaX/distance, deltaY/distance];
+}
