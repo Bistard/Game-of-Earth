cea34d1d5a409e9267904569ace490055b94b354
Charles Zheng <j243zhen@uwaterloo.ca>
2021-09-18 15:14:54 -0400

    create isOverlap

--- src/world/entity/entity.ts
+++ src/world/entity/entity.ts
@@ -1,4 +1,4 @@
-import { IPosition } from "../../common/UI/domNode.js";
+import { IDimension, IPosition, Dimension } from "../../common/UI/domNode.js";
 import { World } from "../world.js";
 
 export enum LivingType {
@@ -32,6 +32,7 @@ export abstract class Entity implements IEntity {
     public readonly id: number;
     public readonly type: EntityType;
     public readonly position: IPosition;
+    public readonly dimension!: IDimension;
 
     constructor(type: EntityType, position: IPosition, parentContainer: HTMLElement, container: HTMLElement) {
         this.id = World.entityID++;
@@ -44,28 +45,48 @@ export abstract class Entity implements IEntity {
          * @readonly maintains the state of World
          */
         
+
         World.entities.push(this);
         switch(type) {
             case LivingType.RABBIT:
                 World.state.count.rabbit++;
+                this.dimension = new Dimension(30, 30);
                 break;
             case LivingType.HUMAN:
                 World.state.count.human++;
+                this.dimension = new Dimension(30, 30);
                 break;
             case LivingType.WOLF:
                 World.state.count.wolf++;
+                this.dimension = new Dimension(30, 30);
                 break;
             case LivingType.BEAR:
                 World.state.count.bear++;
+                this.dimension = new Dimension(30, 30);
                 break;
             case StaticType.GRASS:
                 World.state.count.grass++;
+                this.dimension = new Dimension(30, 30);
                 break;
             case StaticType.CLOUD:
                 World.state.count.cloud++;
+                this.dimension = new Dimension(30, 30);
                 break;
         }
 
+    }
+
+    static isOverlap(m: Entity, n: Entity) : boolean {
+        let mlx : number = m.position.x;
+        let mly : number = m.position.y;
+        let mrx : number = m.position.x + m.dimension.width;
+        let mry : number = m.position.x - m.dimension.height;
+        let nlx : number = n.position.x;
+        let nly : number = n.position.y;
+        let nrx : number = n.position.x + n.dimension.width;
+        let nry : number = n.position.x - n.dimension.height;
+
+        return !((mlx >= nrx || nlx >= mrx) || (mry >= nly || nry >= mly))
     }
 
     public abstract update(): void;
