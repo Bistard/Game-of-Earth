c225b73f3436bbe45c7c010dedf1a8e51fe33494
Junang Li <tonyli1897@gmail.com>
2021-09-18 23:07:43 -0400

    update cloud, grass

--- src/world/entity/grass.ts
+++ src/world/entity/grass.ts
@@ -1,8 +1,12 @@
+import { Browser } from "../../browser/browser.js";
 import { IPosition } from "../../common/UI/domNode.js";
+import { World } from "../world.js";
 import { StaticType } from "./entity.js";
 import { StaticEntity } from "./staticEntity.js";
 
 export class Grass extends StaticEntity {
+    private existTime: number = 0;
+
     constructor(parentContainer: HTMLElement, position: IPosition) {
         super(StaticType.GRASS, position, parentContainer, document.createElement('div'));
         this.parentContainer.appendChild(this.container);
@@ -10,7 +14,16 @@ export class Grass extends StaticEntity {
     }
 
     public override update(): void {
-        
+        this.existTime++;
+        if (this.existTime > 1800) {
+            // pass life time, destroy self
+            for(let i = 0; i < World.entities.length; i++){
+                if (World.entities[i] == this) {
+                    World.entities.splice(i, 1);
+                    break;
+                }
+            }
+        }
     }
 
     protected override _render(): void {
